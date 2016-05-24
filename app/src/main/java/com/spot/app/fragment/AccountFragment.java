7fb3d11553b3b7e01d81d4d8876ba0bd766742fa
ivanhiq@gmail.com <qzg1122>
2016-05-24 19:34:51 +0800

    代码提交---qi

---
+++ app/src/main/java/com/spot/app/fragment/AccountFragment.java
@@ -0,0 +1,30 @@
+package com.spot.app.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.spot.app.R;
+
+/**
+ * 个人中心
+ */
+public class AccountFragment extends Fragment {
+
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/spot/app/fragment/ProofFragment.java
@@ -0,0 +1,30 @@
+package com.spot.app.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.spot.app.R;
+
+/**
+ * 取证.
+ */
+public class ProofFragment extends Fragment {
+
+
+    public ProofFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_proof, container, false);
+    }
+
+}
