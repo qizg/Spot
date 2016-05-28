4ab998675b54cff7b8446310b5065282f5eb2ab3
ivanhiq@gmail.com <qzg1122>
2016-05-28 16:30:46 +0800

    代码实现

--- app/src/main/java/com/spot/app/WelcomeActivity.java
+++ app/src/main/java/com/spot/app/WelcomeActivity.java
@@ -26,9 +26,9 @@ public class WelcomeActivity extends Activity {
         boolean isLogin = sp_Info.getBoolean(Constant.IS_LOGIN, false);
 
 //        if (!isLogin)
-            startActivity(new Intent(this, LoginActivity.class));
+//            startActivity(new Intent(this, LoginActivity.class));
 //        else
-//            startActivity(new Intent(this, HomeActivity.class));
+            startActivity(new Intent(this, HomeActivity.class));
 
         finish();
 
--- app/src/main/java/com/spot/app/activity/HomeActivity.java
+++ app/src/main/java/com/spot/app/activity/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.spot.app.activity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -126,45 +128,15 @@ public class HomeActivity extends AppCompatActivity
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.home, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
