53673890c9eaad83fbef9078d8e17d297edaf16e
qq627955224 <627955224qq>
2016-05-28 13:29:37 +0800

    个人信息页面

--- app/src/main/java/com/spot/app/activity/LoginActivity.java
+++ app/src/main/java/com/spot/app/activity/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.spot.app.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.spot.app.R;
 
 public class LoginActivity extends Activity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton  = (Button) findViewById(R.id.bt_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+            }
+        });
     }
 }
