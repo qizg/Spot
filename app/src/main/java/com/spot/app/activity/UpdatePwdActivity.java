4d36af2b8cf997037f8f7a91c0ae57dd7dba0792
ivanhiq@gmail.com <qzg1122>
2016-05-29 15:19:32 +0800

    代码实现

--- app/src/main/java/com/spot/app/activity/UpdatePwdActivity.java
+++ app/src/main/java/com/spot/app/activity/UpdatePwdActivity.java
@@ -2,6 +2,9 @@ package com.spot.app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 import com.spot.app.R;
 
 public class UpdatePwdActivity extends AppCompatActivity {
@@ -10,5 +13,16 @@ public class UpdatePwdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_pwd);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.update_pwd);
+        toolbar.setNavigationIcon(R.drawable.ps_info_18);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
